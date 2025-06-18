c6b5f6e2680b6deeffc9f5b6efa7c8a82b8ce9ab
Garrett J <36933037+GarrettJardine37@users.noreply.github.com>
2025-06-17 21:29:36 -0600

    added map asset

--- frontend/src/components/WorldMap.tsx
+++ frontend/src/components/WorldMap.tsx
@@ -1,12 +1,20 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, ImageOverlay } from 'react-leaflet'
+
+import nightMap from '../assets/BlackMarble.png';
 
 function WorldMap(props: any) {
+
   return (
     <MapContainer 
-       center={[props.currentLocation.lat, props.currentLocation.lng]} zoom={7} scrollWheelZoom={true} className="w-full h-full">
+       center={[props.currentLocation.lat, props.currentLocation.lng]} zoom={2} scrollWheelZoom={true} className="w-full h-full">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <ImageOverlay
+        bounds={[[90, -90], [-180, 180]]}
+        url={nightMap}
+        opacity={0.5}
       />
       {/* <Marker position={[props.currentLocation.lat, props.currentLocation.lng]}>
         <Popup>
